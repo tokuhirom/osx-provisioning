347af014de953b38565ce0e60e642d5183f46b17
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-04-19 09:55:10 +0900

    initial import

---
+++ install-ansible.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# sudo xcodebuild -license
+
+which pip >/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+  echo "not found command pip"
+  echo "install pip"
+  sudo easy_install pip
+fi
+
+which ansible >/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+  echo "not found command ansible"
+  echo "install ansible"
+  sudo pip install ansible
+fi
